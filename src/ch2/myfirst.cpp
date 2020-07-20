b64106702bb0f2569c1a520eef930d919de7ba5e
jarvis <jiashunming@outlook.com>
2020-07-20 23:07:44 +0800

    first cpp file

---
+++ src/ch2/myfirst.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 贾顺名 on 2020/7/20.
+//
+
+#include "myfirst.h"
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    return 0;
+}
---
+++ src/ch2/myfirst.h
@@ -0,0 +1,14 @@
+//
+// Created by 贾顺名 on 2020/7/20.
+//
+
+#ifndef LEARNING_CPP_MYFIRST_H
+#define LEARNING_CPP_MYFIRST_H
+
+
+class myfirst {
+
+};
+
+
+#endif //LEARNING_CPP_MYFIRST_H
