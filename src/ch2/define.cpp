bc77d5584aa02831b8af46f1e0143393ce470f98
jarvis <jiashunming@outlook.com>
2020-07-23 18:27:38 +0800

    feat: add define template

---
+++ src/ch2/define.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 贾顺名 on 2020/7/23.
+//
+
+#include <iostream>
+
+#define TEST(str)           \
+    cout << (str) << endl;
+
+using namespace std;
+
+int main() {
+    TEST("hello");
+    TEST("world");
+
+    return 0;
+}
