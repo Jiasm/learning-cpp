8516b3f1c3af320b3d5da5257725113e87524e27
jarvis <jiashunming@outlook.com>
2020-07-25 21:49:57 +0800

    feat: add modulus

---
+++ src/ch3/divide.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 贾顺名 on 2020/7/25.
+// integer and floating-point division
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    cout << "Integer division: 9 / 5 = " << 9 / 5 << endl;
+    cout << "Floating-point division: 9.0 / 5.0 = ";
+    cout << 9.0 / 5.0 << endl;
+    cout << "Mixed division: 9.0 / 5 = " << 9.0 / 5 << endl;
+    cout << "double constants: 1e7 / 9.0 = ";
+    cout << 1.e7 / 9.0 << endl;
+    cout << "float constants: 1e7f / 9.0f = ";
+    cout << 1.e7f / 9.0f << endl;
+
+    return 0;
+}
+
---
+++ src/ch3/modulus.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 贾顺名 on 2020/7/25.
+// uses % operator to convert lbs to stone
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    const int Lbs_per_stn = 14;
+    int lbs;
+
+    cout << "Enter your weight in pounds: ";
+    cin >> lbs;
+    int stone = lbs / Lbs_per_stn;
+    int pounds = lbs % Lbs_per_stn;
+
+    cout << lbs << " pounds are " << stone << " stone, " << pounds << " pound(s).\n";
+
+    return 0;
+}
