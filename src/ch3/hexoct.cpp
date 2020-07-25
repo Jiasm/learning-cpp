66ced58b31b750a4fc5aeee7c1886f5f3f98daf6
jarvis <jiashunming@outlook.com>
2020-07-25 16:21:58 +0800

    feat: add hexoct

--- src/ch3/hexoct.cpp
+++ src/ch3/hexoct.cpp
@@ -1,4 +1,21 @@
 //
 // Created by 贾顺名 on 2020/7/25.
+// shows hex and octal literals
 //
 
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int chest = 42;
+    int waist = 0x42;
+    int inseam = 042;
+
+    cout << "Monsieur cuts a striking figure!" << endl;
+    cout << "chest = " << chest << " (42 in decimal)" << endl;
+    cout << "waist = " << waist << " (0x42 in hex)" << endl;
+    cout << "inseam = " << inseam << " (042 in octal)" << endl;
+
+    return 0;
+}
