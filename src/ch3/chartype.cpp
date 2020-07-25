5a71f80d0a6dcf1a05c03c9020ccc45b97ad9bf9
jarvis <jiashunming@outlook.com>
2020-07-25 16:31:18 +0800

    feat: add chartype

---
+++ src/ch3/chartype.cpp
@@ -0,0 +1,20 @@
+//
+// Created by 贾顺名 on 2020/7/25.
+// the char type
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    char ch;
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " character." << endl;
+
+    return 0;
+}
+
