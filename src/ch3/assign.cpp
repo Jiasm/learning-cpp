779bfd3cd127c104344440772934f45550e183d8
jarvis <jiashunming@outlook.com>
2020-07-27 23:00:31 +0800

    feat: add arrayone

---
+++ src/ch3/assign.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 贾顺名 on 2020/7/25.
+// type changes on initialization
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    float tree = 3;
+    int guess(3.9832);
+    int debt = 7.2E12;
+
+    cout << "tree = " << tree << endl;
+    cout << "guess = " << guess << endl;
+    cout << "debt = " << debt << endl;
+
+    return 0;
+}
+
