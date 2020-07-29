3d3755fdbb4fe2bd27141de074b3328d53ebad48
jarvis <jiashunming@outlook.com>
2020-07-29 23:17:00 +0800

    feat: add struct

---
+++ src/ch4/struct.cpp
@@ -0,0 +1,36 @@
+//
+// Created by 贾顺名 on 2020/7/29.
+// a simple structure
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct inflatable {
+    char name[20];
+    float volume;
+    double price;
+};
+
+int main() {
+    inflatable guest = {
+            "Glorious Gloria",
+            1.88,
+            29.99
+    };
+
+    inflatable pal = {
+            "Audacious Arthur",
+            3.12,
+            32.99
+    };
+
+    cout << "Expand your guest list with " << guest.name;
+    cout << " and " << pal.name << "!\n";
+
+    cout << "You can have both for $";
+    cout << guest.price + pal.price << "!\n";
+
+    return 0;
+}
